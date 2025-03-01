0791af02afe34590f4355cad45498ad269fe29f4
TrevorGZ-sys7 <discordtrevor7@gmail.com>
2025-03-01 09:11:30 +0300

    initialization of game

--- invade.py
+++ invade.py
@@ -1,5 +1,18 @@
-def function(a, b):
-    result = a+b
-    print(f"sum is equal to {result}")
+import pygame
 
-function(17, 32)
+pygame.init() #to initialize pygame:)
+
+scree = pygame.display.set_mode((800, 600)) #to access the display
+
+#Tittle and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('spaceship.png')
+pygame.display.set_icon(icon)
+
+# create an event which allows to exit the display
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
