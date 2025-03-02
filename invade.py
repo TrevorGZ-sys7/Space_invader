753d02c7d7ceb1b50c192b137a31df1737ed6868
TrevorGZ-sys7 <discordtrevor7@gmail.com>
2025-03-02 15:47:33 +0300

    more updates on movements

--- invade.py
+++ invade.py
@@ -15,9 +15,15 @@ pygame.display.set_icon(icon)
 playerimg = pygame.image.load('shipp.png') 
 playerX = 370
 playerY = 480
+#ENemy
+enemyimg = pygame.image.load('ufo.png') 
+enemyX = 370
+enenmyY = 480
 
-def player():
-    screen.blit(playerimg,(playerX, playerY ))# blit means to draw
+move_left = move_right = move_up = move_down = False
+
+def player(x, y):
+    screen.blit(playerimg,(x, y ))# blit means to draw
 
 
 # create an event which allows to exit the display
@@ -26,11 +32,34 @@ while running:
 
         #background colour
     screen.fill((0,0,100))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        #event for key strokes to move characters
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                move_left = True
+            elif event.key == pygame.K_RIGHT:
+                move_right = True
+            # when key is released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                move_left = False
+            elif event.key == pygame.K_RIGHT:
+                move_right = False
+            
 
+    if move_left:
+        playerX -= 0.5
+    if move_right:
+        playerX += 0.5
+    
+    if playerX <= 0:
+        playerX = 0
+    if playerX >= 736:
+        playerX = 736
 
-    player()# we call the function here so that it will always appear 
+    player(playerX, playerY)# we call the function here so that it will always appear 
     pygame.display.update()
